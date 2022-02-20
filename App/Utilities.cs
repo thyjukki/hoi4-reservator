437d6ae41257bb20031ccddc0b38e012429239f2
Jussi Joki <judejoki@gmail.com>
2022-02-20 13:36:18 +0200

    add reservation message

--- App/Utilities.cs
+++ App/Utilities.cs
@@ -53,6 +53,11 @@ namespace Reservator
             var bld = new StringBuilder();
 
             bld.Append($"__**Current reservations ({game?.Reservations.Count ?? 0}):**__");
+            bld.Append("Use reactions to reserve a nation, if nation has a player you can also reserve it for coop, in case the player does not join or for experienced priority");
+            bld.Append("Experienced ranked players have a priority on majors.");
+            bld.Append("**Bolded** nations can only be reserved by experienced or intermediate players");
+            bld.Append("If you are new to the server but an **experienced MP player** and wish to play a major, please contact staff member for the rank.");
+            bld.Append("Cooping majors does not currently work for other ranks, use ✋ reaction if you are planing to coop");
 
             await BuildCountryList(ccs, bld, discord, game);
 
